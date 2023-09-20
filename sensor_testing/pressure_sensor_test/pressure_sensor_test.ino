df65f10b57f114a68ef7ccf0ec9d0286f65603d2
adp1030 <aphillips@olin.edu>
2023-09-20 11:05:41 -0400

    Updated pressure test

--- sensor_testing/pressure_sensor_test/pressure_sensor_test.ino
+++ sensor_testing/pressure_sensor_test/pressure_sensor_test.ino
@@ -1,8 +1,11 @@
 int PRESSURE_SENSOR_PIN = A1;
 int sensor_reading;
+float voltage;
+float pressure; //conversion from datasheet
 
 void setup() {
   // put your setup code here, to run once:
+  analogReadResolution(14); //change to 14-bit resolution
   long baudRate = 9600;
   Serial.begin(baudRate);
 }
@@ -11,7 +14,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   sensor_reading = analogRead(PRESSURE_SENSOR_PIN);
-  Serial.print("Sensor Reading:");
-  Serial.print(sensor_reading);
+  voltage = sensor_reading*(5.0/16382.0); //from 5V rail
+  pressure = (voltage/5.0-0.1)/0.6667; //from datasheet in MPa
+
+  Serial.print("Pressure (MPa): ");
+  Serial.println(pressure);
   delay(100); //for readability
 }
