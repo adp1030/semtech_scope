d475badb3d26240245885ff53a4e113937b8ef06
adp1030 <aphillips@olin.edu>
2023-12-05 20:45:44 -0500

    adding more downlink tests

---
+++ jedi_uplink_serial_downlink/serial_downlink/serial_downlink.ino
@@ -0,0 +1,36 @@
+int x;
+
+#include "Arduino_LED_Matrix.h"
+ArduinoLEDMatrix matrix;
+
+//Define LED matrices to switch between
+
+const uint32_t happy[] = {
+    0x19819,
+    0x80000001,
+    0x81f8000
+};
+const uint32_t heart[] = {
+    0x3184a444,
+    0x44042081,
+    0x100a0040
+};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(1);
+  matrix.begin();
+}
+
+void  loop() {
+  while (!Serial.available());
+  x = Serial.readString().toInt();
+
+  if (x == 0){
+    matrix.loadFrame(heart);
+
+  }
+  if (x == 1){
+    matrix.loadFrame(happy);
+  }
+}
---
+++ jedi_uplink_serial_downlink/serial_downlink_high.py
@@ -0,0 +1,10 @@
+# Importing Libraries 
+import serial 
+import time 
+
+#Define Arduino
+arduino = serial.Serial(port='COM10', baudrate=9600, timeout=.1) 
+
+arduino.write(bytes('1', 'utf-8')) 
+time.sleep(0.05) 
+data = arduino.readline() 
