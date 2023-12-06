a48f4d4fbcc97017bdccebaa49fdaa22d62a952c
adp1030 <aphillips@olin.edu>
2023-12-06 14:06:08 -0500

    add downlink signals for actuating valves

--- jedi_uplink_serial_downlink/serial_downlink/serial_downlink.ino
+++ jedi_uplink_serial_downlink/serial_downlink/serial_downlink.ino
@@ -1,36 +1,54 @@
-int x;
+/*
+Serial input key:
+Shutoff low: 0
+Shutoff high: 1
+Drip low: 2
+Drip high: 3
 
-#include "Arduino_LED_Matrix.h"
-ArduinoLEDMatrix matrix;
+Write digital pins, delay for 5 seconds potentially to actuate valve, then set low
+*/
 
-//Define LED matrices to switch between
-
-const uint32_t happy[] = {
-    0x19819,
-    0x80000001,
-    0x81f8000
-};
-const uint32_t heart[] = {
-    0x3184a444,
-    0x44042081,
-    0x100a0040
-};
+int x; //serial input
 
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(1);
-  matrix.begin();
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  //Write enable pins high
+  digitalWrite(2,HIGH);
+  digitalWrite(10,HIGH);
 }
 
 void  loop() {
   while (!Serial.available());
   x = Serial.readString().toInt();
 
-  if (x == 0){
-    matrix.loadFrame(heart);
+  if (x == 0){ //Shutoff low
+    digitalWrite(3, HIGH);
+    delay(6000);
+    digitalWrite(3, LOW);
+  }
+  if (x == 1){ //Shutoff high
+    digitalWrite(4, HIGH);
+    delay(6000);
+    digitalWrite(4, LOW);
+  }
+  if (x == 2){ //Drip low
+    digitalWrite(11, HIGH);
+    delay(6000);
+    digitalWrite(11, LOW);
+  }
+  if (x == 3){ //Drip high
+    digitalWrite(12, HIGH);
+    delay(6000);
+    digitalWrite(12, LOW);
+  }
 
-  }
-  if (x == 1){
-    matrix.loadFrame(happy);
-  }
 }
