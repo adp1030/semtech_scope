96ae547622a698f6b09fb02c6733fe26b0e49d28
Luke Raus <lraus@olin.edu>
2024-02-28 10:39:36 -0500

    Refactor analog conversion

--- harnessed_freezer_tests/analog_to_measurements.py
+++ harnessed_freezer_tests/analog_to_measurements.py
@@ -6,77 +6,71 @@ from thermistor_inline_transduce.inline_thermistor import InlineThermistor
 from thermistor_surface_transduce.surface_thermistor import SurfaceThermistor
 
 
-surface_therm   = SurfaceThermistor()
-inline_therm    = InlineThermistor()
-pressure_sensor = PressureSensor()
 
 
-df = pd.read_csv('Data_2024_02_15_t10_08_45_copy.csv')
+def transduce_raw_data(csv_filename):
+    """
+    Return dataframe of transduced data given CSV of raw analog readings.
+    """        
+    surface_therm   = SurfaceThermistor()
+    inline_therm    = InlineThermistor()
+    pressure_sensor = PressureSensor()
 
-df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y_%m_%d_t%H_%M_%S')
+    df = pd.read_csv(csv_filename)
 
-# # Surface thermistor measurements
-# df['Inlet_surface_res [Ohm]']  = df['A0'].apply(surface_therm.analog_to_res)
-# df['Outlet_surface_res [Ohm]'] = df['A1'].apply(surface_therm.analog_to_res)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y_%m_%d_t%H_%M_%S')
 
-# # Inline thermistor measurements
-# df['Inlet_inline_res [Ohm]']   = df['A2'].apply(inline_therm.analog_to_res)
-# df['Outlet_inline_res [Ohm]']  = df['A3'].apply(inline_therm.analog_to_res)
+    # Surface thermistor measurements
+    df['Temp surface Z.1 [C]'] = df['A0'].apply(surface_therm.analog_to_temp)
+    df['Temp surface Z.3 [C]'] = df['A1'].apply(surface_therm.analog_to_temp)
 
+    # Inline thermistor measurements
+    df['Temp inline Z.1 [C]']  = df['A2'].apply(inline_therm.analog_to_temp)
+    df['Temp inline OUT [C]']  = df['A3'].apply(inline_therm.analog_to_temp)
 
-
-# Surface thermistor measurements
-df['Inlet_surface_temp [C]']  = df['A0'].apply(surface_therm.analog_to_temp)
-df['Outlet_surface_temp [C]'] = df['A1'].apply(surface_therm.analog_to_temp)
-
-# Inline thermistor measurements
-df['Inlet_inline_temp [C]']   = df['A2'].apply(inline_therm.analog_to_temp)
-df['Outlet_inline_temp [C]']  = df['A3'].apply(inline_therm.analog_to_temp)
-
-# Pressure sensor measurements
-df['Inlet_pressure [MPa]']   = df['A4'].apply(pressure_sensor.analog_to_pressure)
-df['Outlet_pressure [MPa]']  = df['A5'].apply(pressure_sensor.analog_to_pressure)
-
-df['Inlet_pressure [psi]']   = df['Inlet_pressure [MPa]']  * 145.038
-df['Outlet_pressure [psi]']  = df['Outlet_pressure [MPa]'] * 145.038
+    # Pressure sensor measurements
+    df['Pressure Z.2 [psi]']   = df['A4'].apply(pressure_sensor.analog_to_pressure_psi)
+    df['Pressure OUT [psi]']   = df['A5'].apply(pressure_sensor.analog_to_pressure_psi)
 
 
-print(df)
+    df['Temp inline OUT [C]'] = df['Temp inline OUT [C]'].where((df['Temp inline OUT [C]'] > -50), other=pd.NA)
+
+    return df
 
 
+def plot_data(df):
 
-# Create subplots
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+    #df = df[0:1000]
+    # Create subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
 
-# Plot temperature
-ax1.plot(df['Timestamp'], df['Inlet_surface_temp [C]'],  label='Inlet_surface')
-ax1.plot(df['Timestamp'], df['Outlet_surface_temp [C]'], label='Outlet_surface')
-ax1.plot(df['Timestamp'], df['Inlet_inline_temp [C]'],   label='Inlet_inline')
-ax1.plot(df['Timestamp'][300:], df['Outlet_inline_temp [C]'][300:], label='Outlet_inline')
-ax1.set_ylabel('Temperature [C]')
-ax1.legend()
+    # Plot temperature
+    ax1.plot(df['Timestamp'], df['Temp surface Z.1 [C]'], label='Zone 1 surface')
+    ax1.plot(df['Timestamp'], df['Temp inline Z.1 [C]'],  label='Zone 1 inline')
+    ax1.plot(df['Timestamp'], df['Temp surface Z.3 [C]'], label='Zone 3 surface')
+    ax1.plot(df['Timestamp'], df['Temp inline OUT [C]'],  label='OUT inline')
+    ax1.set_ylabel('Temperature [C]')
+    ax1.legend()
 
-# Plot pressure
-ax2.plot(df['Timestamp'], df['Inlet_pressure [psi]'],  label='Inlet')
-ax2.plot(df['Timestamp'], df['Outlet_pressure [psi]'], label='Outlet')
-ax2.set_ylabel('Pressure [psi]')
-ax2.set_xlabel('Timestamp')
-ax2.legend()
+    # Plot pressure
+    ax2.plot(df['Timestamp'], df['Pressure Z.2 [psi]'],  label='Zone 2')
+    ax2.plot(df['Timestamp'], df['Pressure OUT [psi]'],  label='OUT')
+    ax2.set_ylabel('Pressure [psi]')
+    ax2.set_xlabel('Timestamp')
+    ax2.legend()
 
-plt.suptitle('Temperature and Pressure Time Series')
-plt.tight_layout()
-plt.show()
+    plt.suptitle('Temperature and Pressure Time Series')
+    plt.tight_layout()
+    plt.show()
 
-# # Plot raw analog values
-# plt.figure(figsize=(10, 6))
-# plt.plot(df['Timestamp'], df['A0'], label='A0')
-# plt.plot(df['Timestamp'], df['A1'], label='A1')
-# plt.plot(df['Timestamp'], df['A2'], label='A2')
-# plt.plot(df['Timestamp'], df['A3'], label='A3')
-# plt.plot(df['Timestamp'], df['A4'], label='A4')
-# plt.plot(df['Timestamp'], df['A5'], label='A5')
-# plt.xlabel('Timestamp')
-# plt.ylabel('Analog')
-# plt.legend()
-# plt.title('Analog vs. time')
-# plt.show()
+
+if __name__ == '__main__':
+
+    df = transduce_raw_data('Data_2024_02_21_t09_30_55_fixed.csv')
+    #df = transduce_raw_data('Data_2024_02_15_t10_08_45.csv')
+    #df = pd.read_csv('Transduced_data.csv')
+
+    print(df)
+    plot_data(df)
+
+    df.to_csv('Transduced_data_drip_test.csv', index=False)
--- harnessed_freezer_tests/pressure_transduce/pressure_sensor.py
+++ harnessed_freezer_tests/pressure_transduce/pressure_sensor.py
@@ -39,15 +39,26 @@ class PressureSensor():
         self.v_in       = v_in
         self.adc_bits   = adc_bits
 
-    def analog_to_pressure(self, analog_int):
+    def analog_to_pressure_mpa(self, analog_int):
         """
-        Return temperature from given analog_int measurement
+        Return temperature in MPa from given analog_int measurement
 
         Inputs:
             analog_int: non-negative integer reported from ADC
         Returns:
-            temp_measured: deg C
+            pressure: float of pressure in MPa
         """
         v_measured = self.v_in * analog_int / ((2**self.adc_bits) - 1)
-        pressure = (v_measured/5.0 - 0.1)/0.6666667    # from datasheet, in MPa
+        pressure = (v_measured/5.0 - 0.1) * 1.5    # from datasheet, in MPa
         return pressure
+
+    def analog_to_pressure_psi(self, analog_int):
+        """
+        Return temperature in PSI from given analog_int measurement
+
+        Inputs:
+            analog_int: non-negative integer reported from ADC
+        Returns:
+            pressure: float of pressure in PSI
+        """
+        return self.analog_to_pressure_mpa(analog_int) * 145.038
